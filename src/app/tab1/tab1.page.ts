8a2ffbbe78326eb64126b711a6589d234a205e86
unknown <adistiraxx77@gmail.com>
2022-10-24 15:16:09 +0700

    update css ion-toolbar, tab1 & tab4

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,15 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  features: any[] = [
+    {id: 1, name: 'Scan this QR Code', src: '/assets/qr1.png', background: 'rgba(255, 196, 9, 0.1)', page: ''}
+  ];
+
+  transactions: any[] = [
+    {id: 1, vendor: 'Received from PhonePe', image: '', amount: 1500, time: '3:00PM'},
+    {id: 2, vendor: 'Flaticons', image: '', amount: -1200, time: '4:00PM'}
+  ];
+
   constructor() {}
 
 }
